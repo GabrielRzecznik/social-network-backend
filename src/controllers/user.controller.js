9a4850c4b4948cf50b0d50fb543cb658df97e0ba
Gabriel Rzecznik <rzecznike@gmail.com>
2025-02-22 14:14:01 -0300

    login and register

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,31 @@
 import User from '../models/user.model.js';
 import { findUserByEmailOrUsername } from "../services/user.service.js";
 import bcrypt from 'bcryptjs';
-//import jwt from 'jsonwebtoken';
+
+export async function registerUser(req, res) {
+  const { name, surname, email, username, password, img_user } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.registerUser({
+      name, surname, email, username, password: hashedPassword, img_user
+    });
+
+    res.status(201).json({ message: 'Usuario registrado', userId: newUser.id_user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error al registrar usuario' });
+  }
+}
 
 export const loginUser = async (req, res) => {
-  const { email, username, password } = req.query;
+  const { email, username, password } = req.body;
+  
   try {
     const user = await findUserByEmailOrUsername(email, username);
     if (!user) return res.status(404).json({ message: "Usuario no encontrado" });
-
+    
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) return res.status(401).json({ message: "Contraseña incorrecta" });
 
@@ -17,14 +34,3 @@ export const loginUser = async (req, res) => {
     res.status(500).json({ message: "Error interno del servidor" });
   }
 };
-
-export async function registerUser(req, res) {
-  try {
-    const { name, surname, email, username, password, img_user } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.createUser({ name, surname, email, username, password: hashedPassword, img_user });
-    res.status(201).json({ message: 'Usuario registrado', userId: newUser.id_user });
-  } catch (error) {
-    res.status(500).json({ error: 'Error al registrar usuario' });
-  }
-}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,6 @@ import { registerUser, loginUser } from '../controllers/user.controller.js';
 
 const router = Router();
 router.post('/register', registerUser);
-router.get('/login', loginUser);
+router.post('/login', loginUser);
 
 export default router;
