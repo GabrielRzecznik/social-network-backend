3c4fb24a78141b3dc39d73fb1490d9c3703b65b0
Gabriel Rzecznik <rzecznike@gmail.com>
2025-02-22 21:57:48 -0300

    updateUser

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -11,4 +11,25 @@ export default class User {
     const result = await pool.query(query, [name, surname, email, username, password, img_user]);
     return result.rows[0];
   }
+
+  static async updateUser(id, { name, surname, email, username, img_user }) {
+    const query = `
+      UPDATE "user"
+      SET name = $1, surname = $2, email = $3, username = $4, img_user = $5
+      WHERE id_user = $6
+      RETURNING id_user, name, surname, email, username, img_user;
+    `;
+    
+    const result = await pool.query(query, [name, surname, email, username, img_user, id]);
+    return result.rows[0];
+  }
+
+  static async getUserById(id) {
+    const query = `
+      SELECT * FROM "user"
+      WHERE id_user = $1;
+    `;
+    const result = await pool.query(query, [id]);
+    return result.rows[0];
+  }
 }
