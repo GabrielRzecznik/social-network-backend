a3ff37b039f11d764627f6df2a61a60ede56f508
Gabriel Rzecznik <rzecznike@gmail.com>
2025-02-23 13:41:13 -0300

    normalize

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,8 +3,8 @@ import { registerUser, loginUser, updateUser } from "../controllers/user.control
 
 const router = Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.put('/:id', updateUser);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.put("/:id", updateUser);
 
 export default router;
