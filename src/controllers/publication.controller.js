0f1d8e02246abdfcc1e53a4ce111097c68303bd5
Gabriel Rzecznik <rzecznike@gmail.com>
2025-02-23 13:41:13 -0300

    normalize

--- src/controllers/publication.controller.js
+++ src/controllers/publication.controller.js
@@ -1,10 +1,10 @@
-import * as PublicationService from "../services/publication.service.js";
+import { createPublication } from "../services/publication.service.js";
 
 export const createNewPublication  = async (req, res) => {
   const { user_id, content } = req.body;
 
   try {
-    const newPublication = await PublicationService.createPublication({ user_id, content });
+    const newPublication = await createPublication({ user_id, content });
     res.status(201).json({ message: "Publicación creada", publication: newPublication });
   } catch (error) {
     res.status(500).json({ message: error.message });
