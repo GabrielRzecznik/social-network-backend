a3541f9d0a4a885e71514a550e738e2b3df5812e
Gabriel Rzecznik <rzecznike@gmail.com>
2025-02-23 13:32:11 -0300

    normalize user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,13 +1,12 @@
 import pool from "../config/db.js";
 
-export default class User {
+class User {
   static async registerUser({ name, surname, email, username, password, img_user }) {
     const query = `
       INSERT INTO "user" (name, surname, email, username, password, img_user)
       VALUES ($1, $2, $3, $4, $5, $6)
       RETURNING id_user, name, surname, email, username, img_user;
     `;
-    
     const result = await pool.query(query, [name, surname, email, username, password, img_user]);
     return result.rows[0];
   }
@@ -19,8 +18,9 @@ export default class User {
       WHERE id_user = $6
       RETURNING id_user, name, surname, email, username, img_user;
     `;
-    
     const result = await pool.query(query, [name, surname, email, username, img_user, id]);
     return result.rows[0];
   }
 }
+
+export default User;
