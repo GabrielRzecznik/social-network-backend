8dfa60a96716b87721e5b10e3b96cb686a2e5ed0
Gabriel Rzecznik <rzecznike@gmail.com>
2025-02-23 18:06:58 -0300

    move SQL queries from service to model for better separation of concerns

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,15 +1,9 @@
-import pool from "../config/db.js";
+import User from "../models/user.model.js";
 
 export const getUserById = async (id) => {
-  const query = 'SELECT * FROM "user" WHERE id_user = $1';
-  const result = await pool.query(query, [id]);
-  return result.rows[0] || null;
+  return await User.getUserById(id);
 };
 
 export const findUserByEmailOrUsername = async (email, username) => {
-  const result = await pool.query(
-    'SELECT * FROM "user" WHERE email = $1 OR username = $2',
-    [email, username]
-  );
-  return result.rows[0] || null;
+  return await User.findByEmailOrUsername(email, username);
 };
