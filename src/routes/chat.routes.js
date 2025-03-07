52f56fa85b67747ad3852c7ab25699aee405dcad
Gabriel Rzecznik <rzecznike@gmail.com>
2025-03-06 21:09:22 -0300

    fix: correct route definitions in chat.routes.js to ensure proper authentication and consistency

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -1,11 +1,11 @@
-import { Router } from "express";
-import { getUserChats, getChat, updateChat } from "../controllers/chat.controller.js";
+import { Router } from 'express';
+import { getUserChats, getChat, updateChat } from '../controllers/chat.controller.js';
 import { protectRoute } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
-router.get("/chat-list", protectRoute, getUserChats);
-router.post("/:id_chat", getChat);
-router.patch("/update", protectRoute, updateChat);
+router.get('chat-list', protectRoute, getUserChats);
+router.post('/:id_chat', protectRoute, getChat);
+router.patch('/update', protectRoute, updateChat);
 
 export default router;
