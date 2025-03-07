dbf00f0fdab2d5fe41e8601c5622e6c564d75d69
Gabriel Rzecznik <rzecznike@gmail.com>
2025-03-06 21:10:10 -0300

    refactor: standardize string quotes in follow.routes.js for consistency

--- src/routes/follow.routes.js
+++ src/routes/follow.routes.js
@@ -1,12 +1,12 @@
-import { Router } from "express";
-import { addFollow, removeFollow, getFollowsCount, getFollowers, getFollowings } from "../controllers/follow.controller.js";
+import { Router } from 'express';
+import { addFollow, removeFollow, getFollowsCount, getFollowers, getFollowings } from '../controllers/follow.controller.js';
 
 const router = Router();
 
-router.post("/add", addFollow);
-router.patch("/remove", removeFollow);
-router.get("/follows-count", getFollowsCount);
-router.get("/followers", getFollowers);
-router.get("/followings", getFollowings);
+router.post('/add', addFollow);
+router.patch('/remove', removeFollow);
+router.get('/follows-count', getFollowsCount);
+router.get('/followers', getFollowers);
+router.get('/followings', getFollowings);
 
 export default router;
