e88c61fd66e1335fb235c081715f88cc880c55c2
Gabriel Rzecznik <rzecznike@gmail.com>
2025-03-09 22:11:22 -0300

    fix: update chat route path from '/update' to '/status' for clarity

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -6,6 +6,6 @@ const router = Router();
 
 router.get('list', protectRoute, getUserChats);
 router.post('/:id_chat', protectRoute, getChat);
-router.patch('/update', protectRoute, updateChat);
+router.patch('/status', protectRoute, updateChat);
 
 export default router;
