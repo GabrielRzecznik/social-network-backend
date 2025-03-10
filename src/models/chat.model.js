ad8b995b23aeeed1ee07dcc5d1e6706f67a0635f
Gabriel Rzecznik <rzecznike@gmail.com>
2025-03-09 22:14:37 -0300

    fix: rename status_chat to status in Chat model for clarity

--- src/models/chat.model.js
+++ src/models/chat.model.js
@@ -1,9 +1,9 @@
 class Chat {
-  constructor({ id_chat, id_user1, id_user2, status_chat }) {
+  constructor({ id_chat, id_user1, id_user2, status }) {
     this.id_chat = id_chat;
     this.id_user1 = id_user1;
     this.id_user2 = id_user2;
-    this.status_chat = status_chat;
+    this.status = status;
   }
 }
 
